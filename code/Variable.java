c03e191fcd2401853d305959660b890d1dcc06ef
41chaitanya <chaitanya4141sharma@gmail.com>
2024-12-07 12:11:56 +0530

    pushing variables and solution of firstProgram Practice set

---
+++ code/Variable.java
@@ -0,0 +1,38 @@
+public class Variable {
+    int var=12;
+
+    public static void main(String[] args) {
+        // // variable
+
+        // // declare
+        //     // <datatype> <var Name>;
+        //     int myNumber;
+        // // define
+        // myNumber=5;
+        // // use
+        // System.out.println(myNumber);
+        
+
+        // // 2 steps
+        // int myVar=45;
+        
+        // System.out.println(myVar);
+
+        
+        // // System.out.println();
+
+
+        // types of variable 
+        // 1 > local var
+        {
+            int myVar=15;
+            System.out.println(myVar);
+            {
+                
+                System.out.println(myVar);
+            }
+        }
+
+        
+    }
+}
